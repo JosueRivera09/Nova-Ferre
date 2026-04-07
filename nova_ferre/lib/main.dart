0455b7f31ee2f01efa276d7faae9f8ab3e71059f
josue <jsuerivera09@gmail.com>
2026-04-06 18:43:40 -0600

    initial commit

---
+++ nova_ferre/lib/main.dart
@@ -0,0 +1,27 @@
+import 'nova_ferre_exports.dart';
+
+void main() {
+  runApp(const NovaFerreApp());
+}
+
+class NovaFerreApp extends StatelessWidget {
+  const NovaFerreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NovaFerre POS',
+
+      // Configuración básica del Tema (Dark Mode)
+      /*theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: const Color(0xFF155DFC), // Tu azul vibrante
+      ),
+      */
+      // Apuntamos directo al MainLayout que vas a crear
+      home: const MainLayout(),
+    );
+  }
+}
---
+++ nova_ferre/lib/nova_ferre_exports.dart
@@ -0,0 +1,8 @@
+// paquetes necesarios
+export 'package:flutter/material.dart';
+
+// views
+export 'ui/main_layout.dart';
+
+//paquetes para estilo y utilidades
+export 'package:google_fonts/google_fonts.dart';
